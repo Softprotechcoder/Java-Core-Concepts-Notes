8aac355beef1757b9b256bf95a6f976d36a81817
Ayush Vaibhav <softprotechcoder@outlook.com>
2026-08-30 00:20:39 +0530

    removed insted class and used anonymous class with enhanced for

--- Collections/ManualIterator.java
+++ Collections/ManualIterator.java
@@ -7,11 +7,14 @@ public class ManualIterator {
         String[] names = {"Ayush","Sanjana","Aryan","Rohit"};
         NameContainer nameContainer = new NameContainer(names);
 
-        Iterator<String> it = nameContainer.iterator();
+        // Iterator<String> it = nameContainer.iterator();
         
-            while(it.hasNext()){
-                System.out.println(it.next());
-            }
+        //     while(it.hasNext()){
+        //         System.out.println(it.next());
+        //     }
+        for (String name : nameContainer){
+            System.out.println(name);
+        }
     }
 }
 class NameContainer implements Iterable<String>{
@@ -25,10 +28,36 @@ class NameContainer implements Iterable<String>{
 
     @Override
     public Iterator<String> iterator() {
-       return new NameContainerIterator();
-    }
-    private class NameContainerIterator implements Iterator<String>{
 
+      
+        // return new NameContainerIterator(); // here we are returning the object of internal class which implements iterator interface.
+
+          /* Used Anonymous class removed NameContainerIterator class */
+       return new Iterator<String>(){
+
+         int position = 0;
+
+        @Override
+        public boolean hasNext() {
+          return position < size;
+        }
+
+        @Override
+        public String next() {
+           return names[position++];
+        }
+
+       };
+
+
+    }
+
+    /* 
+     private class NameContainerIterator implements Iterator<String>{
+    
+    // here as we know this internal class is used once it just overrides method of iterator interface.
+    // and as we know if the class is used once we can replace it with anonymous class.
+    
         int position = 0;
 
         @Override
@@ -42,4 +71,6 @@ class NameContainer implements Iterable<String>{
         }
         
     }
+    */
+   
 }
